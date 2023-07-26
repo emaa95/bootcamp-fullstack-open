1188bb07a35a12cc6ede1092e1689a35ab27af98
emaa95 <chilettiemanuel@gmail.com>
2023-07-26 17:21:01 -0300

    course-content-part 2-3
    se agrego componente total utilizando el método reduce

--- parte2/course-contents/src/components/Course.js
+++ parte2/course-contents/src/components/Course.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
-
+import Total from './Total'
 function Course({course}) {
   
     return (
     <div>
         <Header course={course.name}></Header>
-        <Content parts={course.parts}></Content>        
+        <Content parts={course.parts}></Content> 
+        <Total parts={course.parts}></Total>  
     </div>
   )
 }
---
+++ parte2/course-contents/src/components/Total.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Total({parts}) {
+    const total = parts.reduce((sum,part)=> sum + part.exercises, 0)
+    return (
+        <div>
+            <p>total of {total} exercises</p>
+        </div>
+    )
+}
+
+export default Total
