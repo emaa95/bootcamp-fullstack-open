aea6556952e7c27e457445f97b580b6d38512105
emaa95 <chilettiemanuel@gmail.com>
2023-07-26 17:55:19 -0300

    cour-content- final part
    Se adapto la app para que reciba varios cursos

--- parte2/course-contents/src/components/Course.js
+++ parte2/course-contents/src/components/Course.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from './Header'
 import Content from './Content'
 import Total from './Total'
-function Course({course}) {
+function Course({courses}) {
   
     return (
+    
     <div>
-        <Header course={course.name}></Header>
-        <Content parts={course.parts}></Content> 
-        <Total parts={course.parts}></Total>  
+      {courses.map( course => (
+        <div key={course.id}> 
+          <Header course={course.name}></Header>
+          <Content parts={course.parts}></Content> 
+          <Total parts={course.parts}></Total>  
+        </div>
+      ) )}
+        
     </div>
   )
 }
