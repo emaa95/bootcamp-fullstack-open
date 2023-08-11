57328b26399ba114e0e096d81134d7ed76ca1995
emaa95 <chilettiemanuel@gmail.com>
2023-08-11 18:06:22 -0300

    Exercise 2.15 - phone-book
    parte7 - agregar los nuevos contactos en el backend

--- parte2/phone-book/src/App.js
+++ parte2/phone-book/src/App.js
@@ -44,8 +44,11 @@ function App() {
       number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
+    axios.post('http://localhost:3001/persons', personObject)
+    .then(response =>{
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    })
   }
   }
 
