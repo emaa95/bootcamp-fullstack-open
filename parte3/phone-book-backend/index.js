c8c588fc54e7793cd1115041aadf065661eb1e76
emaa95 <chilettiemanuel@gmail.com>
2023-08-15 18:20:39 -0300

    phone-book-backend-parte4
    ruta para eliminar una persona de la agenda

--- parte3/phone-book-backend/index.js
+++ parte3/phone-book-backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 
 // Datos hardcodeados de la agenda telefónica
-const agendaTelefonica = [
+let agendaTelefonica = [
   { id: 1, nombre: 'Juan', telefono: '123456789' },
   { id: 2, nombre: 'María', telefono: '987654321' },
   { id: 3, nombre: 'Pedro', telefono: '555555555' }
@@ -38,6 +38,22 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end()
     }
 });
+
+// Ruta para eliminar una entrada de la agenda telefónica por su ID
+app.delete('/api/persons/:id', (req, res) => {
+    const idToDelete = parseInt(req.params.id);
+  
+    const entryToDelete = agendaTelefonica.find(entry => entry.id === idToDelete);
+  
+    if (!entryToDelete) {
+      return res.status(404).json({ error: 'Entrada no encontrada' });
+    }
+  
+    agendaTelefonica = agendaTelefonica.filter(entry => entry.id !== idToDelete);
+  
+    res.status(204).end();
+  });
+  
 
 app.listen(port, () => {
   console.log(`Servidor iniciado en http://localhost:${port}`);
