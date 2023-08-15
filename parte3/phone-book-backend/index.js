b513e90bbbe0ddd98ff040ac4c5bb0b59bc76047
emaa95 <chilettiemanuel@gmail.com>
2023-08-15 17:25:59 -0300

    Exercise 3.1 phone-book-backend
    parte 1 y 2

---
+++ parte3/phone-book-backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+// Datos hardcodeados de la agenda telefónica
+const agendaTelefonica = [
+  { id: 1, nombre: 'Juan', telefono: '123456789' },
+  { id: 2, nombre: 'María', telefono: '987654321' },
+  { id: 3, nombre: 'Pedro', telefono: '555555555' }
+];
+
+// Ruta para obtener la lista de personas
+app.get('/api/persons', (req, res) => {
+  res.json(agendaTelefonica);
+});
+
+app.get('/info', (req,res) => {
+    const currentTime = new Date();
+    const entryCount = agendaTelefonica.length
+
+    const infoResponse = `
+    <h2>Información de la solicitud</h2>
+    <p>Hora de la solicitud: ${currentTime}</p>
+    <p>Cantidad de entradas en la agenda: ${entryCount}</p>
+    `;
+
+    res.send(infoResponse);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
