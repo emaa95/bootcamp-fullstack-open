4d0609e32a1292fffa440492c285411a82c67a5b
emaa95 <chilettiemanuel@gmail.com>
2023-08-28 17:37:29 -0300

    Exercise 4.2 - bloglist
    Refactorización de código en módulos.

--- parte4/bloglist/index.js
+++ parte4/bloglist/index.js
@@ -1,52 +1,7 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const app = require('./app') 
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Conexión exitosa a la base de datos');
-
-    
-  })
-  .catch(err => {
-    console.error('Error en la conexión a la base de datos:', err);
-  });
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
