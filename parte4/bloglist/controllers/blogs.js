ccf5a0ff47013bd6240ca704f739a9f2175dcc7c
ema <chilettiemanuel@gmail.com>
2023-09-12 11:29:11 -0300

    Bloglist - administración de usuarios
    Funcionando

--- parte4/bloglist/controllers/blogs.js
+++ parte4/bloglist/controllers/blogs.js
@@ -1,10 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then( blogs => {
-        response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await 
+    Blog.find({}).populate('user', {username: 1 , name: 1})
+    
+    response.json(blogs)
+    
 })
 
 blogsRouter.get('/:id', (request, response, next) => {
@@ -19,22 +22,29 @@ blogsRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
+    const user = await User.findById(body.userId)
 
+    if (!user) {
+        // El usuario no fue encontrado en la base de datos, puedes enviar un mensaje de error o manejarlo de acuerdo a tus necesidades.
+        return response.status(404).json({ error: 'Usuario no encontrado' });
+    }
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: user._id
     }
     )
 
-    blog.save()
-    .then(savedBlog => {
-        response.json(savedBlog)
-    })
-    .catch(error => next(error))
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.json(savedBlog)
 })
 
 blogsRouter.delete('/:id', (request, response, next) => {
