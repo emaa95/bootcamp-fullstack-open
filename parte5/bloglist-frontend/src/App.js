3344fdfbe56fa0372db02a91e9f278174f25149b
ema <chilettiemanuel@gmail.com>
2023-10-02 16:34:10 -0300

    Exercise 5.9 - Bloglist-frontend
    Los blogs ahora se muestran por cantidad de likes.

--- parte5/bloglist-frontend/src/App.js
+++ parte5/bloglist-frontend/src/App.js
@@ -147,7 +147,7 @@ const App = () => {
         <BlogForm createBlog={addBlog}></BlogForm>
         </Togglable>
         <h2>blogs</h2>
-        {blogs.map(blog =>
+        {blogs.sort((a, b) => (b.likes - a.likes)).map(blog =>
         <Blog key={blog.id} blog={blog} addBlogLike={addBlogLike} />
         )}
         </div>
