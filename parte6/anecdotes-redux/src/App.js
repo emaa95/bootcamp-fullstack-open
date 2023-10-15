34d615bd1d453964aa01fc1b231cb74f960042ca
ema <chilettiemanuel@gmail.com>
2023-10-14 21:14:25 -0300

    Exercise 6.7 - anecdotes-redux
    separación de la lógica de creación de una anécdota en su propio componente.

--- parte6/anecdotes-redux/src/App.js
+++ parte6/anecdotes-redux/src/App.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-
-const getId = () => (100000 * Math.random()).toFixed(0)
+import { addVote} from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,25 +8,12 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
-    dispatch({
-      type: 'VOTE',
-      data: {id}
-    })
+    dispatch(
+      addVote(id)
+    )
   }
 
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch({
-      type: 'NEW_ANECDOTE',
-      data: {
-        content,
-        votes: 0,
-        id: getId()
-      }
-    })
-  }
+  
 
   return (
     <div>
@@ -42,11 +29,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name ="anecdote" /></div>
-        <button type= "submit">create</button>
-      </form>
+      <AnecdoteForm></AnecdoteForm>
     </div>
   )
 }
