9aa121dc74367157eb09da133306f27eb8214507
ema <chilettiemanuel@gmail.com>
2023-11-02 17:48:01 -0300

    Exercise 8.9 - library - frontend
    se agrego una consulta para obtener el título, autor y año de publicación de los libros desde el servidor. La vista de libros funciona correctamente.

--- parte8/library-frontend/src/components/Books.js
+++ parte8/library-frontend/src/components/Books.js
@@ -1,10 +1,19 @@
+import { useQuery } from "@apollo/client"
+import React from "react"
+import { ALL_BOOKS } from "../queries"
+
 const Books = (props) => {
+
+    const result = useQuery(ALL_BOOKS) 
+
     if (!props.show) {
       return null
     }
-  
-    const books = []
-  
+    
+    if (result.loading){
+        return <div>loading...</div>
+    }
+    
     return (
       <div>
         <h2>books</h2>
@@ -16,7 +25,7 @@ const Books = (props) => {
               <th>author</th>
               <th>published</th>
             </tr>
-            {books.map((a) => (
+            {result.data.allBooks.map((a) => (
               <tr key={a.title}>
                 <td>{a.title}</td>
                 <td>{a.author}</td>
