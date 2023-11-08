f193bbb905ac8692e0abdf12d95424afa5178b2b
ema <chilettiemanuel@gmail.com>
2023-11-08 18:35:27 -0300

    Exercise 8.19 - library - frontend
    se cambio la vista de libros para que ahora se puedan filtrar con respecto a su género.

--- parte8/library-frontend/src/components/Books.js
+++ parte8/library-frontend/src/components/Books.js
@@ -1,41 +1,62 @@
 import { useQuery } from "@apollo/client"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { ALL_BOOKS } from "../queries"
 
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+  const [books, setBooks] = useState([])
+  const [selectedGenre, setSelectedGenre] = useState('All genres')
 
-    const result = useQuery(ALL_BOOKS) 
+  useEffect(() => {
+    if (result.data) {
+      const allBooks = result.data.allBooks
+      setBooks(allBooks)
+    }
+  }, [result])
 
-    if (!props.show) {
-      return null
-    }
-    
-    if (result.loading){
-        return <div>loading...</div>
-    }
-    
-    return (
-      <div>
-        <h2>books</h2>
-  
-        <table>
-          <tbody>
-            <tr>
-              <th></th>
-              <th>author</th>
-              <th>published</th>
-            </tr>
-            {result.data.allBooks.map((a) => (
-              <tr key={a.title}>
-                <td>{a.title}</td>
-                <td>{a.author.name}</td>
-                <td>{a.published}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
+  const uniqueGenres = [...new Set(['All genres', ...books.map(book => book.genres).flat()])]
+
+  const filteredBooks = selectedGenre === 'All genres' ? books : books.filter(b => b.genres.includes(selectedGenre))
+
+  if (!props.show) {
+    return null
   }
-  
-  export default Books
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>books</h2>
+      <p>
+        in genre <b>{selectedGenre}</b>
+      </p>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>author</th>
+            <th>published</th>
+          </tr>
+          {filteredBooks.map((book) => (
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+      {uniqueGenres.map((genre) => (
+        <button onClick={() => setSelectedGenre(genre)} key={genre}>
+        {genre}
+        </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Books
