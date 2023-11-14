0e57b8672564a23b61b88b21dd6ce276e3ee197a
ema <chilettiemanuel@gmail.com>
2023-11-14 16:55:53 -0300

    Exercise 9.5 - first - steps
    se configuro la ruta /bmi que se le pasa por la url los parametros a la función y devuelvo por un json el resultado.  También se gestionan los errores.

--- parte9/first-steps/index.ts
+++ parte9/first-steps/index.ts
@@ -1,8 +1,32 @@
 import express from 'express';
+import { calculateBmi, parseBmiArguments } from './bmiCalculator';
+
 const app = express();
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+
+  if (!weight || !height) {
+    res.status(400);
+    res.send({ error: 'malformatted parameters' });
+  } else {
+    try {
+      const { heightInCm, weightInKg } = parseBmiArguments(
+        Number(height),
+        Number(weight)
+      );
+      const bmi = calculateBmi(heightInCm, weightInKg);
+      res.send({
+        weight: weightInKg,
+        height: heightInCm,
+        bmi: bmi
+      });
+    } catch (e) {
+      res.status(400);
+      res.send({ error: e.message });
+    }
+  }
 });
 
 const PORT = 3003;
