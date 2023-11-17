79d87ef2fba5487c74b4f910865fb514a4c9837c
ema <chilettiemanuel@gmail.com>
2023-11-17 16:53:51 -0300

    Exercise 9.7 - first - steps
    se agrego una solicitud post de la calculadora de ejercicios. Funciona todo correctamente.

--- parte9/first-steps/index.ts
+++ parte9/first-steps/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { calculateBmi, parseBmiArguments } from './bmiCalculator';
+import { calculateExercises, parseExerciseArguments } from './exerciseCalculator';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/bmi', (req, res) => {
-  const height = Number(req.query.height)
-  const weight = Number(req.query.weight)
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
 
   if (!weight || !height) {
     res.status(400);
@@ -23,9 +26,43 @@ app.get('/bmi', (req, res) => {
         bmi: bmi
       });
     } catch (e) {
-      res.status(400);
-      res.send({ error: e.message });
+        if (e instanceof Error) {
+            res.status(400).send({ error: e.message });
+          } else {
+            res.status(500).send({ error: 'Internal Server Error' });
+          }
     }
+  }
+});
+
+app.post('/exercises', (req, res) => {
+  
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const dailyExercises = req.body.exerciseHours;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const dailyTarget = req.body.target;
+
+  if ( !dailyTarget || !dailyExercises ) {
+    return res.status(400).send({ error: "parameters missing" });
+  }  else {
+    try{
+      const { target, exerciseHours } = parseExerciseArguments(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          dailyTarget,
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          dailyExercises
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const result = calculateExercises(target, exerciseHours);
+      return res.send({result});
+    }
+    catch (e) {
+      if (e instanceof Error) {
+          return res.status(400).send({ error: e.message });
+        } else {
+          return res.status(500).send({ error: 'Internal Server Error' });
+        }
+  }
   }
 });
 
