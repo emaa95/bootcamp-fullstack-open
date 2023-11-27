a19f6887b1842154fa819b3b743193896f311cb1
ema <chilettiemanuel@gmail.com>
2023-11-27 18:32:10 -0300

    Exercise 9.21 - patientor - frontend
    se agrego un componente para la vista de de un paciente en particular. Además se configuro una ruta para cada paciente.

---
+++ parte9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Patient } from '../../types';
+import { useParams } from 'react-router-dom';
+import Icon from '@mui/material/Icon';
+import MaleIcon from '@mui/icons-material/Male';
+import FemaleIcon from '@mui/icons-material/Female';
+import { faGenderless } from '@fortawesome/free-solid-svg-icons/faGenderless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+interface PatientDetailsProps {
+    patients: Patient[];
+}
+
+const PatientDetails : React.FC<PatientDetailsProps> = ({patients}) => {
+    
+    const { id } = useParams<{ id: string }>();
+
+
+    const patient = Object.values(patients).find(
+        (patient: Patient) => patient.id === id
+      );
+    
+    let iconName: React.ReactElement;
+
+    if (patient) {
+        switch(patient.gender) {
+            case 'male':
+              iconName = <MaleIcon/>;
+              break;
+            
+            case 'female':
+              iconName = <FemaleIcon/>;
+              break;
+            case 'other':
+              iconName = <FontAwesomeIcon icon={faGenderless} />;
+              break;
+            default:
+              iconName = <FemaleIcon/>;
+          }
+        
+        return (
+            <div>
+            <h2>
+              {patient.name}<Icon>{''}{iconName}</Icon> 
+            </h2>
+            <p>ssh: {patient.ssn}</p>
+            <p>occupation: {patient.occupation}</p>
+            </div>
+        );
+    }
+    return null;
+};
+
+export default PatientDetails;
