18ee3cbfd62bb1f7f121b6782c844810f61cb3a8
ema <chilettiemanuel@gmail.com>
2023-11-27 11:30:50 -0300

    Exercise 9.18 - flight - frontend
    se configuraron las notificaciones para casos de éxito y fracaso de una solicitud post de un vuelo. Se puede acceder a los errores del backend correctamente.

--- parte9/flight-diary-frontend/src/components/FlightForm.tsx
+++ parte9/flight-diary-frontend/src/components/FlightForm.tsx
@@ -5,6 +5,9 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import { ToastContainer, toast } from 'react-toastify';
+import axios, { AxiosError } from 'axios';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface FormProps {
   onNewFlight: (newFlight: Flight) => void;
@@ -27,6 +30,37 @@ const FlightForm: React.FC<FormProps> = ({ onNewFlight }) => {
     });
   };
 
+  const handleAxiosError = (error: AxiosError) => {
+  if (error.response) {
+    // El servidor respondió con un código de estado fuera del rango 2xx
+    console.error('Error de respuesta del servidor:', error.response.data);
+
+    // Acceder al mensaje específico del backend (si está disponible)
+    const backendErrorMessage = (error.response.data as { message?: string })?.message;
+
+    // Mostrar notificación de error con el mensaje específico del backend
+    toast.error(backendErrorMessage ?? 'Error al crear el vuelo');
+
+    // Puedes ajustar la duración y otras opciones según tus necesidades
+  } else if (error.request) {
+    // La solicitud fue hecha, pero no se recibió respuesta
+    console.error('No se recibió respuesta del servidor');
+
+    // Mostrar notificación de error
+    toast.error('Error al crear el vuelo');
+
+    // Puedes ajustar la duración y otras opciones según tus necesidades
+  } else {
+    // Algo sucedió al configurar la solicitud que desencadenó un error
+    console.error('Error al configurar la solicitud:', error.message);
+
+    // Mostrar notificación de error
+    toast.error('Error al crear el vuelo');
+
+    // Puedes ajustar la duración y otras opciones según tus necesidades
+  }
+};
+
   const flightCreation = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
@@ -38,12 +72,38 @@ const FlightForm: React.FC<FormProps> = ({ onNewFlight }) => {
         visibility: Visibility.Good,
         comment: '',
       });
+
+      toast.success('Vuelo creado con exito', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        })
     } catch (error) {
-      console.error('Error al crear el vuelo', error);
+      if (axios.isAxiosError(error)) {
+        handleAxiosError(error)
+      } else {
+        toast.error('Error desconocido al crear el vuelo');
+      }
     }
   };
 
   return (
+    <div>
+    <ToastContainer position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"/>
     <form onSubmit={flightCreation}>
       <FormControl>
         <FormLabel>Date</FormLabel>
@@ -87,6 +147,7 @@ const FlightForm: React.FC<FormProps> = ({ onNewFlight }) => {
 
       <Button type="submit">Submit</Button>
     </form>
+    </div>
   );
 };
 
