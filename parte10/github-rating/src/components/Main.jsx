95ea5839e1eaa327cc53c1a8c6088027fe887891
ema <chilettiemanuel@gmail.com>
2023-12-05 17:11:19 -0300

    Exercise 10.3 - github - rating
    este componente contiene a los demás y es el principal.

---
+++ parte10/github-rating/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import RepositoryList from './RepositoryList';
+
+const Main = () => {
+  return (
+        <View>
+          <Text>Rate Repositorty Application</Text>
+          <RepositoryList/>
+        </View>
+  );
+};
+
+export default Main;
