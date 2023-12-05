49d4402de0950764d7ca8b2abe2e31523fc77e65
ema <chilettiemanuel@gmail.com>
2023-12-05 11:57:54 -0300

    Exercise 10.2 - github - rating
    se configuro eslint correctamente.

---
+++ parte10/github-rating/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    'standard'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    semi: ['error', 'always'],
+    'no-unused-vars': 'off'
+  }
+};
